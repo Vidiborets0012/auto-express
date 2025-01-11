47439e3da421a8f6f3fa755130e69fef639a8a80
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-11 13:16:20 +0200

    adapt the carousel section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ $(function () {
     $('.carousel-inner').slick({
         arrows: false,
         dots: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2                    
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1                    
+                }
+            },
+        ]
     });
 
 });
